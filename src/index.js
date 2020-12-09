0aed0e1fdd8d09f7e2074bb0460bd59e46b3ff88
Michelle Lin <michelleamazinglin@gmail.com>
2020-12-09 09:36:39 -0500

    remove some stuff

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles/index.scss";
-// import "./scripts/map";
+import "./scripts/map";
 import "./scripts/canvas";
+import "./scripts/player"
+
 
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,409 +1,3 @@
-let ctx = null;
-
-// 地图： 0-墙壁 1-草地 2-陆地 4-河流
-let gameMap = [
-            0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 2, 2, 2, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 1, 1, 2, 1, 0, 0, 0, 0, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 1, 1, 2, 1, 0, 2, 2, 0, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 1, 1, 2, 1, 0, 2, 2, 0, 4, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
-			0, 1, 1, 2, 2, 2, 2, 2, 0, 4, 4, 4, 1, 1, 1, 0, 2, 2, 2, 0,
-			0, 1, 1, 2, 1, 0, 2, 2, 0, 1, 1, 4, 1, 1, 1, 0, 2, 2, 2, 0,
-			0, 1, 1, 2, 1, 0, 2, 2, 0, 1, 1, 4, 1, 1, 1, 0, 2, 2, 2, 0,
-			0, 1, 1, 2, 1, 0, 0, 0, 0, 1, 1, 4, 1, 1, 0, 0, 0, 2, 0, 0,
-			0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 2, 2, 2, 2, 0,
-			0, 1, 1, 2, 2, 2, 2, 2, 2, 1, 4, 4, 1, 1, 0, 2, 2, 2, 2, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 0, 2, 2, 2, 2, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 0, 2, 2, 2, 2, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 0, 2, 0, 0, 0, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-];
-
-let tileMap = new TileMap();
-
-//定义屋顶位置， x就是格子
-let roofList = [
-	{ x:5, y:3, w:4, h:7, data: [
-		10, 10, 11, 11,
-		10, 10, 11, 11,
-		10, 10, 11, 11,
-		10, 10, 11, 11,
-		10, 10, 11, 11,
-		10, 10, 11, 11,
-		10, 10, 11, 11
-	]},
-	{ x:15, y:5, w:5, h:4, data: [
-		10, 10, 11, 11, 11,
-		10, 10, 11, 11, 11,
-		10, 10, 11, 11, 11,
-		10, 10, 11, 11, 11
-	]},
-	{ x:14, y:9, w:6, h:7, data: [
-		10, 10, 10, 11, 11, 11,
-		10, 10, 10, 11, 11, 11,
-		10, 10, 10, 11, 11, 11,
-		10, 10, 10, 11, 11, 11,
-		10, 10, 10, 11, 11, 11,
-		10, 10, 10, 11, 11, 11,
-		10, 10, 10, 11, 11, 11
-	]}
-];
-
-let tileWidth = 40,tileHeight = 40; //定义每个方块尺寸
-let mapWidth = 20,mapHeight = 20; //定义地图大小 （看上面的array）
-let currentSecond = 0,frameCount = 0,framesLastSecond = 0,lastFrameTime = 0;
-
-//宣告圖資雪碧圖
-let tileset = null,
-  tilesetURL = "https://i.postimg.cc/yNhPZ9NT/tileset.png",
-  tilesetLoaded = false;
-
-let gameTime = 0;
-
-//宣告移動速度
-let gameSpeeds = [
-  { name: "Normal", mult: 1 },
-  { name: "Slow", mult: 0.3 },
-  { name: "Fast", mult: 3 },
-  { name: "Paused", mult: 0 }
-];
-let currentSpeed = 0;
-
-//動態物件宣告------------------------------------------------------------
-function Sprite(data) {
-  this.animated = data.length > 1; //資料大於1動態呈現
-  this.frameCount = data.length; //資料偵數
-  this.duration = 0; //持續時間
-  this.loop = true;
-
-  if (data.length > 1) {
-    for (let i in data) {
-      if (typeof data[i].d == "undefined") {
-        data[i].d = 100;
-      }
-      this.duration += data[i].d;
-
-      if (typeof data[i].loop != "undefined") {
-        this.loop = data[i].loop ? true : false;
-      }
-    }
-  }
-
-  this.frames = data;
-}
-
-Sprite.prototype.draw = function(t, x, y) {
-  let frameIdx = 0;
-
-  if (!this.loop && this.animated && t >= this.duration) {
-    frameIdx = this.frames.length - 1;
-  } else if (this.animated) {
-    t = t % this.duration;
-    let totalD = 0;
-
-    for (let i in this.frames) {
-      totalD += this.frames[i].d;
-      frameIdx = i;
-
-      if (t <= totalD) {
-        break;
-      }
-    }
-  }
-
-  let offset =
-    typeof this.frames[frameIdx].offset == "undefined"
-      ? [0, 0]
-      : this.frames[frameIdx].offset;
-
-  ctx.drawImage(
-    tileset,
-    this.frames[frameIdx].x,
-    this.frames[frameIdx].y,
-    this.frames[frameIdx].w,
-    this.frames[frameIdx].h,
-    x + offset[0],
-    y + offset[1],
-    this.frames[frameIdx].w,
-    this.frames[frameIdx].h
-  );
-};
-
-let itemTypes = {
-  1: {
-    name: "Star",
-    maxStack: 2,
-    sprite: new Sprite([{ x: 240, y: 0, w: 40, h: 40 }]),
-    offset: [0, 0]
-  }
-};
-
-function Stack(id, qty) {
-  this.type = id;
-  this.qty = qty;
-}
-function Inventory(s) {
-  this.spaces = s;
-  this.stacks = [];
-}
-Inventory.prototype.addItems = function(id, qty) {
-  for (let i = 0; i < this.spaces; i++) {
-    if (this.stacks.length <= i) {
-      let maxHere = qty > itemTypes[id].maxStack ? itemTypes[id].maxStack : qty;
-      this.stacks.push(new Stack(id, maxHere));
-
-      qty -= maxHere;
-    } else if (
-      this.stacks[i].type == id &&
-      this.stacks[i].qty < itemTypes[id].maxStack
-    ) {
-      let maxHere = itemTypes[id].maxStack - this.stacks[i].qty;
-      if (maxHere > qty) {
-        maxHere = qty;
-      }
-
-      this.stacks[i].qty += maxHere;
-      qty -= maxHere;
-    }
-
-    if (qty == 0) {
-      return 0;
-    }
-  }
-
-  return qty;
-};
-
-function PlacedItemStack(id, qty) {
-  this.type = id;
-  this.qty = qty;
-  this.x = 0;
-  this.y = 0;
-}
-PlacedItemStack.prototype.placeAt = function(nx, ny) {
-  if (tileMap.map[toIndex(this.x, this.y)].itemStack == this) {
-    tileMap.map[toIndex(this.x, this.y)].itemStack = null;
-  }
-
-  this.x = nx;
-  this.y = ny;
-
-  tileMap.map[toIndex(nx, ny)].itemStack = this;
-};
-
-let objectCollision = {
-  none: 0,
-  solid: 1
-};
-let objectTypes = {
-  1: {
-    name: "Box",
-    sprite: new Sprite([{ x: 40, y: 160, w: 40, h: 40 }]),
-    offset: [0, 0],
-    collision: objectCollision.solid,
-    zIndex: 1
-  },
-  2: {
-    name: "Broken Box",
-    sprite: new Sprite([{ x: 40, y: 200, w: 40, h: 40 }]),
-    offset: [0, 0],
-    collision: objectCollision.none,
-    zIndex: 1
-  },
-  3: {
-    name: "Tree top",
-    sprite: new Sprite([{ x: 80, y: 160, w: 80, h: 80 }]),
-    offset: [-20, -20],
-    collision: objectCollision.solid,
-    zIndex: 3
-  }
-};
-function MapObject(nt) {
-  this.x = 0;
-  this.y = 0;
-  this.type = nt;
-}
-MapObject.prototype.placeAt = function(nx, ny) {
-  if (tileMap.map[toIndex(this.x, this.y)].object == this) {
-    tileMap.map[toIndex(this.x, this.y)].object = null;
-  }
-
-  this.x = nx;
-  this.y = ny;
-
-  tileMap.map[toIndex(nx, ny)].object = this;
-};
-
-let floorTypes = {
-  solid: 0,
-  path: 1,
-  water: 2,
-  ice: 3,
-  conveyorU: 4,
-  conveyorD: 5,
-  conveyorL: 6,
-  conveyorR: 7,
-  grass: 8
-};
-let tileTypes = {
-  0: {
-    colour: "#685b48",
-    floor: floorTypes.solid,
-    sprite: new Sprite([{ x: 0, y: 0, w: 40, h: 40 }])
-  },
-  1: {
-    colour: "#5aa457",
-    floor: floorTypes.grass,
-    sprite: new Sprite([{ x: 40, y: 0, w: 40, h: 40 }])
-  },
-  2: {
-    colour: "#e8bd7a",
-    floor: floorTypes.path,
-    sprite: new Sprite([{ x: 80, y: 0, w: 40, h: 40 }])
-  },
-  3: {
-    colour: "#286625",
-    floor: floorTypes.solid,
-    sprite: new Sprite([{ x: 120, y: 0, w: 40, h: 40 }])
-  },
-  4: {
-    colour: "#678fd9",
-    floor: floorTypes.water,
-    sprite: new Sprite([
-      { x: 160, y: 0, w: 40, h: 40, d: 200 },
-      { x: 200, y: 0, w: 40, h: 40, d: 200 },
-      { x: 160, y: 40, w: 40, h: 40, d: 200 },
-      { x: 200, y: 40, w: 40, h: 40, d: 200 },
-      { x: 160, y: 40, w: 40, h: 40, d: 200 },
-      { x: 200, y: 0, w: 40, h: 40, d: 200 }
-    ])
-  },
-  5: {
-    colour: "#eeeeff",
-    floor: floorTypes.ice,
-    sprite: new Sprite([{ x: 120, y: 120, w: 40, h: 40 }])
-  },
-  6: {
-    colour: "#cccccc",
-    floor: floorTypes.conveyorL,
-    sprite: new Sprite([
-      { x: 0, y: 40, w: 40, h: 40, d: 200 },
-      { x: 40, y: 40, w: 40, h: 40, d: 200 },
-      { x: 80, y: 40, w: 40, h: 40, d: 200 },
-      { x: 120, y: 40, w: 40, h: 40, d: 200 }
-    ])
-  },
-  7: {
-    colour: "#cccccc",
-    floor: floorTypes.conveyorR,
-    sprite: new Sprite([
-      { x: 120, y: 80, w: 40, h: 40, d: 200 },
-      { x: 80, y: 80, w: 40, h: 40, d: 200 },
-      { x: 40, y: 80, w: 40, h: 40, d: 200 },
-      { x: 0, y: 80, w: 40, h: 40, d: 200 }
-    ])
-  },
-  8: {
-    colour: "#cccccc",
-    floor: floorTypes.conveyorD,
-    sprite: new Sprite([
-      { x: 160, y: 200, w: 40, h: 40, d: 200 },
-      { x: 160, y: 160, w: 40, h: 40, d: 200 },
-      { x: 160, y: 120, w: 40, h: 40, d: 200 },
-      { x: 160, y: 80, w: 40, h: 40, d: 200 }
-    ])
-  },
-  9: {
-    colour: "#cccccc",
-    floor: floorTypes.conveyorU,
-    sprite: new Sprite([
-      { x: 200, y: 80, w: 40, h: 40, d: 200 },
-      { x: 200, y: 120, w: 40, h: 40, d: 200 },
-      { x: 200, y: 160, w: 40, h: 40, d: 200 },
-      { x: 200, y: 200, w: 40, h: 40, d: 200 }
-    ])
-  },
-
-  10: {
-    colour: "#ccaa00",
-    floor: floorTypes.solid,
-    sprite: new Sprite([{ x: 40, y: 120, w: 40, h: 40 }])
-  },
-  11: {
-    colour: "#ccaa00",
-    floor: floorTypes.solid,
-    sprite: new Sprite([{ x: 80, y: 120, w: 40, h: 40 }])
-  }
-};
-
-function Tile(tx, ty, tt) {
-  this.x = tx;
-  this.y = ty;
-  this.type = tt;
-  this.roof = null;
-  this.roofType = 0;
-  this.eventEnter = null;
-  this.object = null;
-  this.itemStack = null;
-}
-
-function TileMap() {
-  this.map = [];
-  this.w = 0;
-  this.h = 0;
-  this.levels = 4;
-}
-TileMap.prototype.buildMapFromData = function(d, w, h) {
-  this.w = w;
-  this.h = h;
-
-  if (d.length != w * h) {
-    return false;
-  }
-
-  this.map.length = 0;
-
-  for (let y = 0; y < h; y++) {
-    for (let x = 0; x < w; x++) {
-      this.map.push(new Tile(x, y, d[y * w + x]));
-    }
-  }
-
-  return true;
-};
-
-//添加屋頂
-TileMap.prototype.addRoofs = function(roofs) {
-  for (let i in roofs) {
-    let r = roofs[i];
-
-    if (
-      r.x < 0 ||
-      r.y < 0 ||
-      r.x >= this.w ||
-      r.y >= this.h ||
-      r.x + r.w > this.w ||
-      r.y + r.h > this.h ||
-      r.data.length != r.w * r.h
-    ) {
-      continue;
-    }
-
-    for (let y = 0; y < r.h; y++) {
-      for (let x = 0; x < r.w; x++) {
-        let tileIdx = (r.y + y) * this.w + r.x + x;
-
-        this.map[tileIdx].roof = r;
-        this.map[tileIdx].roofType = r.data[y * r.w + x];
-      }
-    }
-  }
-};
 
 //定義方向
 let directions = {
@@ -462,7 +56,7 @@ let player = new Character();
 //定義角色函式
 function Character() {
   this.tileFrom = [1, 1];
-  this.tileTo = [4, 6]; //角色起始位置（ 地圖相對位置 ）
+  this.tileTo = [1, 1]; //角色起始位置（ 地圖相對位置 ）
   this.timeMoved = 0;
   this.dimensions = [30, 30]; //角色尺寸
   this.position = [40 * this.tileTo[0] + 5, 40 * this.tileTo[1] + 5];
@@ -850,6 +444,33 @@ function drawGame() {
       );
     }
   }
+
+  ctx.textAlign = "right";
+
+  for (let i = 0; i < player.inventory.spaces; i++) {
+    ctx.fillStyle = "#ddccaa";
+    ctx.fillRect(10 + i * 50, 350, 40, 40);
+
+    if (typeof player.inventory.stacks[i] != "undefined") {
+      let it = itemTypes[player.inventory.stacks[i].type];
+
+      it.sprite.draw(gameTime, 10 + i * 50 + it.offset[0], 350 + it.offset[1]);
+
+      if (player.inventory.stacks[i].qty > 1) {
+        ctx.fillStyle = "#000000";
+        ctx.fillText(
+          "" + player.inventory.stacks[i].qty,
+          10 + i * 50 + 38,
+          350 + 38
+        );
+      }
+    }
+  }
+  ctx.textAlign = "left";
+
+  ctx.fillStyle = "#ff0000";
+  ctx.fillText("FPS: " + framesLastSecond, 10, 20);
+  ctx.fillText("Game speed: " + gameSpeeds[currentSpeed].name, 10, 40);
 
   lastFrameTime = currentFrameTime;
   requestAnimationFrame(drawGame);
